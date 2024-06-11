3f3aee541e1145b584965c01eee7d1bb80edc86e
Samuele <samueletasciotti@icloud.com>
2024-06-11 15:02:34 +0200

    feat: element from html / const = 'lista'

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const listaSpesa = ['pane', 'latte', 'uova', 'burro', 'salame'];
 
 // DEBUG
 console.log(listaSpesa);
-// Otteniamo il riferimento all'elemento ul
 
+// Otteniamo il riferimento all'elemento ul
+const listaElement = document.getElementById('lista');
 // Inizializziamo un indice
 
 // Usiamo un ciclo while per  la lista e aggiungiamo ogni elemento alla pagina
