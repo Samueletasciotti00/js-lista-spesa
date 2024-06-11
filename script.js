336cf863e4c64451258892c17cd22b1f95019f4e
Samuele <samueletasciotti@icloud.com>
2024-06-11 16:56:55 +0200

    fix: grammar error to (Legth)

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 // Definiamo la lista della spesa
 const listaSpesa = ['pane', 'latte', 'uova', 'burro', 'salame'];
 
-// DEBUG
-console.log(listaSpesa);
 
 // Otteniamo il riferimento all'elemento ul
-const listaElement = document.getElementById('lista');
+const listaElement = document.getElementById('lista-spesa');
+
 // Inizializziamo un indice
+let i = 0;
 
 // Usiamo un ciclo while per  la lista e aggiungiamo ogni elemento alla pagina
+while (i < listaSpesa.length) {
+    // Creazione elemento della lista
+    const prodotto = document.createElement('li');
+
+    //Contenuto del prdotto dall'Array
+    prodotto.textContent = listaSpesa[i];
+
+    //Append dell prodotto alla lista
+    listaElement.appendChild(prodotto);
+
+    //Incremento del contatore
+    i++;
+    
+}
+
+console.log(i);
