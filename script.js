1e8794d87d980b66eccdad5ff6d5452e6885ccac
Samuele <samueletasciotti@icloud.com>
2024-06-11 14:57:04 +0200

    feat: commit for js program

---
+++ script.js
@@ -0,0 +1,7 @@
+// Definiamo la lista della spesa
+
+// Otteniamo il riferimento all'elemento ul
+
+// Inizializziamo un indice
+
+// Usiamo un ciclo while per  la lista e aggiungiamo ogni elemento alla pagina
